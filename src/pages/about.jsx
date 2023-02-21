8f45ce16a0f99fe8780d8d19fca606b9ccb97026
Jordan Wallace-Addai <jordanwallace1223@gmail.com>
2023-02-21 17:30:36 -0500

    first commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,15 @@
+
+function about() {
+  return (
+    <div>
+    <h1 className="text-6xl mb-4">Github Finder</h1>
+
+    <p className="mb-4 text-2xl font-light">A React App to search Github profiles and see profile details. All apart of the Udemy Course!</p>
+    <p className="text-lg text-gray-400">Version <span className="text-white">1.0.0</span></p>
+    <p className="text-lg text-gray-400">Layout By:
+    <a href="https://twitter.com/hassibmoddasser" className="text-white"> Hassib Moddasser</a></p>
+    </div>
+  )
+}
+
+export default about
---
+++ src/pages/home.jsx
@@ -0,0 +1,8 @@
+
+function home() {
+  return (
+    <h1 className="text-6xl mb-4">Welcome</h1>
+  )
+}
+
+export default home
