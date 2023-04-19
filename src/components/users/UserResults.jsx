db9e4cb092f64f8e5b3ab177c8c191c0492e85e0
Jordan Wallace-Addai <jordanwallace1223@gmail.com>
2023-04-18 20:13:06 -0400

    added a useContext, updated token  and fetched and displayed users from github API

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,36 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useContext } from 'react'
+import UserItem from '../users/UserItem'
 import Spinner from '../layout/Spinner'
+import GithubContext from '../../context/github/GithubContext'
 
 function UserResults() {
-const [users, setUsers] = useState([])
-const [loading, setLoading] = useState(true)
+  // Bring in the Context
+const {users, loading, fetchUsers} = useContext(GithubContext)
 
 // Fetch Users when page loads
 useEffect(() => {
 fetchUsers()
 }, [])
 
-// Fetch GH Users from API
-// Send Token to increase req
-const fetchUsers = async () => {
-  const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-    headers: {
-      Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    },
-  })
-  const data = await res.json()
-
-  setUsers(data)
-  setLoading(false)
-}
-
-// Check in req is loading
+// Check if req is loading
 if(!loading) {
   return (
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
       {/* Map through user login and get login */}
       {users.map((user)=>(
-        <h3>{user.login}</h3>
+        <UserItem key={user.id} user={user} />
     ))}</div>
   )
 } else {
